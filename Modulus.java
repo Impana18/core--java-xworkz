e864af5ef8b1fd3e7194990326c329c2d7ab1708
Impana18 <impanasringeri@gmail.com>
2023-07-13 19:00:26 +0530

    methods

---
+++ Modulus.java
@@ -0,0 +1,17 @@
+class Modulus{
+public static void main(String mod[]){
+mod(45 , 78,78);
+mod(23 , 56,876);
+mod(56 , 12,567);
+mod(21 , 89,543);
+mod(43 , 98,345);
+mod(12 , 67,567);
+mod(13 , 45,345);
+mod(32 , 19,234);
+mod(56 , 67,987);
+mod(39 , 90,000);
+}
+public static void mod(float a, float b, float c){
+System.out.println(a%b%c);
+}
+}
---
+++ Subtraction.java
@@ -0,0 +1,18 @@
+class Subtraction{
+
+public static void main(String subtraction[]){
+sub(67 , 45 ,234);
+sub(624 , 875,87);
+sub(52 , 99,123);
+sub(234 , 67,654);
+sub(123 , 456,8765);
+sub(789 , 345,123);
+sub(234 , 111,12);
+sub(491 , 654,654);
+sub(91 , 67,765);
+sub(567 , 123,123);
+}
+public static void sub(int a , int b, int c){
+System.out.println(a-b-c);
+}
+}
